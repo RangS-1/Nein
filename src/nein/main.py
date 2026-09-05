bff9946fe6e516c4d2e03dafa51f1c1df815261f
RangS-1 <rangga19sj@gmail.com>
2026-09-05 14:13:40 +0700

    Fix: window scroll not crashing!

--- src/nein/main.py
+++ src/nein/main.py
@@ -13,11 +13,31 @@ def cli() -> None:
     filename = parser.parse_args().filename
     curses.wrapper(main, filename)
 
+def update_scroll(y, x, scroll_y, scroll_x, height, width):
+    text_height = height - 2
+    text_width = width - 1
+
+    # Vertical scrolling
+    if y < scroll_y:
+        scroll_y = y
+
+    elif y >= scroll_y + text_height:
+        scroll_y = y - text_height + 1
+
+    # Horizontal scrolling
+    if x < scroll_x:
+        scroll_x = x
+
+    elif x >= scroll_x + text_width:
+        scroll_x = x - text_width + 1
+
+    return scroll_y, scroll_x
 
 def main(stdscr, filename):
     curses.curs_set(1)
     stdscr.nodelay(False)
-    
+    scroll_y, scroll_x = 0, 0
+
     if os.path.exists(filename):
         with open(filename, 'r') as f:
             box = f.read().splitlines() # Semacam Buffer lah ya
@@ -33,9 +53,20 @@ def main(stdscr, filename):
         height, width = stdscr.getmaxyx()
 
         #Rendering
-        for idx, line in enumerate(box):
-            if idx < height - 2: # Sisakan ruang untuk baris status
-                stdscr.addstr(idx, 0, line[:width-1])
+        for screen_y in range(height - 2):
+            file_y = scroll_y + screen_y
+
+            if file_y >= len(box):
+                break
+            
+            line = box[file_y]
+
+            visible_line = line[scroll_x:scroll_x + width - 1]
+
+            try:
+                stdscr.addstr(screen_y, 0, visible_line)
+            except curses.error:
+                pass
 
         #Menu
         try:
@@ -48,15 +79,21 @@ def main(stdscr, filename):
         except curses.error:
             pass 
 
-        # 3. Pindahkan kursor
-        stdscr.move(y, x)
-        
-        # 4. Input Keyboard
-        key = stdscr.getch()
+        # Cursor Position
+        scroll_y, scroll_x = update_scroll(
+            y, x,
+            scroll_y, scroll_x,
+            height, width
+        )
 
+        screen_y = y - scroll_y
+        screen_x = x - scroll_x
+        stdscr.move(screen_y, screen_x)
+        
+        # Keyboard Input
+        key = stdscr.getch()
         if key == 24: # CTRL+X (Exit)
             break
-        
         elif key == 19: # CTRL+S (Save)
             try:
                 with open(filename, "w") as f:
@@ -64,31 +101,26 @@ def main(stdscr, filename):
                 msg = f"Saved to {filename}! "
             except Exception as e:
                 msg = f"Error saving: {e} "
-
         elif key == curses.KEY_UP: #UP
             if y > 0:
                 y -= 1
                 x = min(x, len(box[y]))
-        
         elif key == curses.KEY_DOWN: #D
             if y < len(box) - 1:
                 y += 1
                 x = min(x, len(box[y]))
-
         elif key == curses.KEY_LEFT: #L
             if x > 0:
                 x -= 1
             elif y > 0:
                 y -= 1
                 x = len(box[y])
-
         elif key == curses.KEY_RIGHT:
             if x < len(box[y]):
                 x += 1
             elif y < len(box) - 1: # Pindah ke awal baris berikutnya
                 y += 1
                 x = 0
-
         elif key in (8, 127, curses.KEY_BACKSPACE):
             if x > 0:
                 box[y] = box[y][:x-1] + box[y][x:]
@@ -99,28 +131,24 @@ def main(stdscr, filename):
                 box.pop(y)
                 y -= 1
                 x = old_x
-
         elif key in (10, 13): # Enter
             enter = box[y]
             box[y] = enter[:x]
             box.insert(y + 1, enter[x:])
             y += 1 # Kebawah / tombol enter 1 baris
             x = 0
-
         elif key == curses.KEY_PPAGE: #P UP
             if x > 0:
                 x -= 1
             elif y > 0:
                 y -= 5
                 x = len(box[y])
-
         elif key == curses.KEY_NPAGE: #P DOWN
             if x > 0:
                 x += 1
             elif y > 0:
                 y += 5
                 x = len(box[y])
-
         elif 32 <= key <= 126:
             box[y] = box[y][:x] + chr(key) + box[y][x:]
             x += 1
